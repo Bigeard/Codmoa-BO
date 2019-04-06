ba1f9afb079bbd9f36ad6f8c4e881696923f03fa
Bigeard <robin.bigeard@gmail.com>
2019-04-06 17:12:05 +0200

    Commit change file

--- pages/modifyDb.php
+++ pages/modifyDb.php
@@ -4,6 +4,12 @@
     <a href="./requests.php" class="return">Return</a>
     <h1>Modify Database</h1>
 
+    <div class="buttons-wrapper">
+        <a href="./createSchema.php"><button>Create Schema</button></a>
+        <a href="./removeSchema.php"><button>Remove Schema</button></a>
+        <a href="./createTable.php"><button>Create Table</button></a>
+        <a href="./removeTable.php"><button>Remove Table</button></a>
+    </div>
 </body>
 
 </html>
