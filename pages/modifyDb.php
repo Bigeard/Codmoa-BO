c79ca64643ee341e23ca8429a75c2f1d241adb06
Bigeard <robin.bigeard@gmail.com>
2019-04-06 17:18:31 +0200

    retire les buttons

--- pages/modifyDb.php
+++ pages/modifyDb.php
@@ -5,10 +5,10 @@
     <h1>Modify Database</h1>
 
     <div class="buttons-wrapper">
-        <a href="./createSchema.php"><button>Create Schema</button></a>
-        <a href="./removeSchema.php"><button>Remove Schema</button></a>
-        <a href="./createTable.php"><button>Create Table</button></a>
-        <a href="./removeTable.php"><button>Remove Table</button></a>
+        <a href="./createSchema.php">Create Schema</a>
+        <a href="./removeSchema.php">Remove Schema</a>
+        <a href="./createTable.php">Create Table</a>
+        <a href="./removeTable.php">Remove Table</a>
     </div>
 </body>
 
