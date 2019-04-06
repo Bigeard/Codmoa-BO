6d42fdba592329aececaafde1358aa598d051ad7
adrienvaucard <adrien.vaucard@gmail.com>
2019-04-06 17:12:41 +0200

    WIP Remove Schema

--- pages/removeSchema.php
+++ pages/removeSchema.php
@@ -13,17 +13,17 @@ $users = $api->selectAllUsers();
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Remove User</title>
+    <title>Remove Schema</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="../styles/main.css" />
     <!--<script src="main.js"></script>-->
 </head>
 
 <body>
-    <h1>Remove User</h1>
+    <h1>Remove Schema</h1>
 
     <form action="../library/processing.php" method="POST">
-        <select name="remove_user">
+        <select name="remove_schema">
             <?php foreach ($users as $user) { ?>
                 <option value="<?= $user->usename ?>"><?= $user->usename ?></option>
             <?php } ?>
